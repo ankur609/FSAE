c6f902a575ac056ef2f9c69294526e088d0d5413
ankur609 <38076761+ankur609@users.noreply.github.com>
2018-04-04 20:44:49 +0530

    Add files via upload

---
+++ _26-07.ino
@@ -0,0 +1,442 @@
+
+/*
+ Data acquistion:
+ master arduino 
+ signals descriptions:
+                                        frame description 
+ voltageLV            A0                      A
+ Temperature sensor   A1  (motor)             B  
+ Acceleration pedal1  A2                      C
+ Acceleration pedal2  A3                      D
+ Brake pedal -->      A4                      E
+ Current sensor -->   A5 (+/- 75A channel)    F  // channel d
+ Current sensor -->   A6 (+/- 500A channel)   G   // channel b
+ currentvalue   --->                          H
+ voltage sensor -->   A7                      I
+ speedCar                                     J
+ motorRPM                                     K
+ CurrentLV                                    L
+ amb. temperature                             M                                
+ powerLV                                      N
+ energyLV                                     O
+ SOCLV                                        P
+ powerHV                                      Q
+ energyHV                                     R
+ SOCHV                                        S  
+ 
+
+ functions:
+ 1.data logging
+ 2. communication with dashboard ECU
+ 3. on board arduino nano communication (to get aceleration /speed)
+
+ Errors:
+ 1.SD card initlisiation failed
+ 
+ 
+ Functions:
+ convert(String);
+ Update()
+ 
+*/
+#include <avr/io.h> 
+#include <avr/interrupt.h>
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
+// objects
+File myFile,myFile1;
+Sd2Card card;
+
+
+
+//global variables
+ double value;
+ 
+ const float gearRatio=5;
+ const float radiusTire=.256;
+ String motorRPM= "0.000";
+ String speedCar= "0.000"; //kmhr 
+ String currentLV= "0.000";
+ String ambTemp= "25"; //degree celisius
+ String ene="";
+ 
+ volatile unsigned long pulses=0;
+ volatile unsigned long count=0;
+ volatile float value_speed=0;
+ unsigned int Tcount=10;// no. of overflow timer2
+ 
+unsigned long long pasttime,curtime;
+ 
+ //master
+ String scr="";
+ float currentValue=8.0;
+ //String ambTemp="-1.0";
+ //float powerLV=0; 
+ //float voltageLV=0;                        
+ //float energyLV=0;                       
+ //float SOCLV=0;                           
+ float powerHV=0;
+ float voltageHV=0;                                      
+ float energyHV=0;                                     
+ //float SOCHV=0;                            
+ //String dashboard;
+ bool flag_update=0;
+ int flg=0;
+ bool curflag=0;
+ //name of file
+ const int chipSelect = 10;
+ char Name[15],Name1[15];
+ long long timer=0;
+ String T_ms="";
+ uint8_t flag; //
+float t1;
+ 
+ /*
+  * ISR (INT1_vect)
+ {
+  EIFR&=(~(1<<1));
+  pulses++;
+ }
+ */
+
+ ISR (INT1_vect)
+ {
+ pulses++;
+ }
+ ISR (TIMER2_OVF_vect)  // timer0 overflow interrupt
+{
+  //event to be exicuted every 
+  // clear TOV2
+  TIFR2&=(0xFE);
+  if(count<30)
+  count++;
+  else{
+    count=0;
+    value_speed=pulses;
+    pulses=0;
+  }
+
+
+  
+}
+
+void setup(){
+
+  //debug led
+  //DDRB|=(1<<5);
+//init serial
+Serial.begin(57600);
+
+//init hardware interupt
+ pinMode(3, INPUT);
+// attachInterrupt(digitalPinToInterrupt(3), speedISR,RISING);
+/*
+DRDD|=(1<<0); // int1 input
+EIMSK|=(1<<1);//enable int1
+*/
+//interruptv
+
+EICRA|=(1<<3)|(1<<2);// rising interrupt at int1
+EIMSK|=(1<<1);
+//enable global interrupt variable
+SREG|=(1<<7);
+//init timer (prescaler 1024)
+TCCR2A=0x00;
+TCCR2B=0x00;
+TCCR2B|=(1<<0)|(1<<1)|(1<<2);
+TIMSK2=(1<<0);//overflow interrupt enable
+
+//
+  pinMode(2,OUTPUT);
+ //sd card initialisation 
+ //chip select pin D10
+  pinMode(10, OUTPUT);
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    //Serial.println("Card failed, or not present");
+    // don't do anything more:
+    digitalWrite(2,HIGH);
+    return;
+  }
+  //Serial.println("card initialized.");
+  digitalWrite(2,LOW);
+  delay(100);
+  int value =1;
+  String filename ="Y";
+  filename+=(char)(value/100+'0');
+  filename+=(char)(((value/10)%10)+'0');
+  filename+=(char)((value%10)+'0');
+  filename+=".csv";
+  filename.toCharArray(Name,15);
+ // Check to see if the file exists:
+  if (SD.exists(Name)) {
+   //Serial.println("file exists.");
+  }
+  else {
+    //Serial.println("file doesn't exist.");
+    // open a new file and immediately close it:
+  //Serial.println("Creating example.txt...");
+  myFile = SD.open(Name, FILE_WRITE);
+  myFile.close();
+  }
+  int value1 =1;
+  String filename1 ="Erg";
+  filename1+=(char)(value1/100+'0');
+  filename1+=(char)(((value1/10)%10)+'0');
+  filename1+=(char)((value1%10)+'0');
+  filename1+=".csv";
+  filename1.toCharArray(Name1,15);
+ // Check to see if the file exists:
+  if (SD.exists(Name1)) {
+   //Serial.println("file exists.");
+  }
+  else {
+    //Serial.println("file doesn't exist.");
+    // open a new file and immediately close it:
+  //Serial.println("Creating example.txt...");
+  myFile1 = SD.open(Name1, FILE_WRITE);
+  myFile1.close();
+  }
+  //sd data row intialisation
+  //String sd_string="voltageLV,Temperature Motor,THROTTLE1,THROTTLE2,Brake,current CH1,curent CH2,currentSensor,VOLTAGE,motorRPM,speedCar,currentLV,ambTemp,PowerLV,EnergyLV,SOCLV,PowerHV,EnergyHv,SOCHV,TIME(sec)";
+  String sd_string="vLV,Temp,T1,T2,Brk,cur1,cur2,cur,vHV,mRPM,spe,curLV,TIME(sec)";
+  //String sd_string="test,best";
+  myFile = SD.open(Name, FILE_WRITE);
+  // if the file opened okay, write to it:
+  if (myFile) {
+    //Serial.print("Writing to test.txt...");
+    myFile.println(sd_string);
+    // close the file:
+    myFile.close();
+   //Serial.println("done.");
+   digitalWrite(2,LOW);
+  } else {
+    // if the file didn't open, print an error:
+    digitalWrite(2,HIGH);
+   //Serial.println("error opening file");
+  }
+
+// Creating file for Storing energy,soc data
+ pasttime=millis(); 
+}
+void loop(){
+
+  // calculate Speed
+  value_speed=value_speed/(12.0*.01632*30);
+  motorRPM=String(value_speed*60*gearRatio);
+  speedCar=String(value_speed*2.0*3.14*radiusTire*18.0/5.0/1000);
+
+  
+
+ //read analog values
+   
+
+  String sd_string="";
+  String dashboard=""; 
+  
+  //a0 LV Voltage
+  dashboard+='A';
+  average(0);
+  value=value*26.7/4.7;
+  //voltageLV=value;
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+
+  //a1 Temperature Sensor
+  dashboard+='B';
+  average(1);
+  value=(value*2200.0)/(12.0-value);
+  value=(-3.92+sqrt(3.92*3.92-4.0*0.01101*(498.0-value)))/(2.0*0.01101);
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+  
+  //a2 Acc p1
+  dashboard+='C';
+  average(2);
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+ 
+  //a3 Acc p2
+  dashboard+='D';
+  average(3);
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+  
+  myFile = SD.open(Name, FILE_WRITE);
+  myFile.print(sd_string);
+  //Serial.println(sd_string);
+  
+  
+  sd_string="";
+  //a4 Brake pedal
+  dashboard+='E';
+  average(4);
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+  
+  //a5 Current sensor1 500A Channel B
+  //dashboard+='F';
+  average(6);
+  value=(-268.2868)*value+664.5354;
+  sd_string+=convert(value);
+  //dashboard+=convert(value);
+  if(value>-60&&value<60)
+  curflag=0;
+  else {curflag=0;
+    currentValue=value;  }
+  sd_string+=",";
+    
+  //a6 Current sensor 2 75A Channel
+  //dashboard+='G';
+  average(5);
+  value=(-39.2156)*value+95.349+4.6;
+    sd_string+=convert(value);
+  //dashboard+=convert(value);
+  if(curflag==1) currentValue=value;
+  sd_string+=","; 
+  
+  //current value
+  dashboard+="FGH";
+  sd_string+=convert(currentValue);
+  dashboard+=convert(currentValue);
+  sd_string+=",";
+
+  //  Serial.println(sd_string);
+  
+  myFile.print(sd_string);
+  sd_string="";
+
+  
+  //a7 voltAGE sensor
+  dashboard+='I';
+  average(7);
+  value=200.0/11.0*value-10;
+  voltageHV=value;
+  sd_string+=convert(value);
+  dashboard+=convert(value);
+  sd_string+=",";
+  
+  //motorRPM
+  dashboard+='J';
+  sd_string+=speedCar;
+  dashboard+=speedCar;
+  sd_string+=",";
+  
+  //speedCar
+  dashboard+='K';
+  sd_string+=String(value_speed);
+  dashboard+=String(value_speed);
+  sd_string+=",";
+  
+  //currentLV
+  dashboard+='L';
+  sd_string+=currentLV;
+  dashboard+=currentLV;
+  sd_string+=",";
+/*  
+  //amb. temperature
+  dashboard+='M';
+  sd_string+=ambTemp;
+  dashboard+=ambTemp;
+  sd_string+=",";
+
+  powerLV=conv2float(currentLV)*voltageLV;
+  dashboard+='N';
+  sd_string+=convert(powerLV);
+  dashboard+=convert(powerLV);
+  
+  
+
+  t1=millis()-pasttime;
+  energyLV+=powerLV*t1/1000000;
+  dashboard+='O';
+  sd_string+=convert(energyLV);
+  dashboard+=convert(energyLV);
+  sd_string+=",";
+
+  
+  dashboard+='P';
+  SOCLV=(voltageLV-19.0)*100.0/6.0;
+  sd_string+=convert(SOCLV);
+  dashboard+=convert(SOCLV);
+  sd_string+=",";
+
+*/
+  powerHV=currentValue*voltageHV;
+ // dashboard+='Q';
+ // sd_string+=convert(powerHV);
+ // dashboard+=convert(powerHV);
+ // sd_string+=",";
+
+  curtime=millis();
+  t1=(curtime-pasttime)/1000;
+  pasttime=curtime;
+  energyHV+=powerHV*(t1)/1000;
+ // dashboard+='R';
+ // sd_string+=convert(energyHV);
+  ene=convert(energyHV);
+ // sd_string+=",";
+/*
+  dashboard+='S';
+  SOCHV=(voltageHV-68)*100/14;
+  sd_string+=convert(SOCHV);
+  dashboard+=convert(SOCHV);
+  sd_string+=",";
+  */  
+  dashboard+='A';  
+  float time1 = millis()/1000.0;
+  sd_string+=convert(time1);
+  myFile.println(sd_string);
+  myFile.close();
+  myFile1 = SD.open(Name1, FILE_WRITE);
+  myFile1.println(ene);
+  myFile1.close();
+ //   Serial.println(sd_string); 
+//delay(300);
+//dashcomm();
+  Serial.write('a');
+  Serial.print(dashboard);
+  Serial.write('b');
+
+//Serial.println(sd_string);  
+  delay(300);
+  
+}
+
+// average
+void average(int pin)
+{value=0;
+  for(int i=1;i<=5;i++)
+  value+= 5*analogRead(pin)/1024.0;
+  value=value/5.0;
+ // value=2.05;
+}
+//convert to string
+String convert(double value1)
+{ String s="";
+  long value;
+  if (value1==0) {s="0.000";return s;}
+  else if(value1<0) value=-value1*1000;
+  else value=value1*1000;
+  int ctr=0;
+  while(value!=0)
+  {
+  ctr=ctr+1;
+  int r=value%10;
+  char c=r+'0';
+  s=c+s;
+  if(ctr==3)
+  {
+    s='.'+s;
+  }
+  value=value/10;
+  }
+  if(value1<0) return '-'+s;
+  else return s; 
+}
---
+++ dashboard_13march.ino
@@ -0,0 +1,428 @@
+#include <UTFT.h>
+# include<UTFT_tinyFAT.h>
+# include<URTouch.h>
+# include<UTFT_Geometry.h>
+# include<EEPROM.h>
+int addr=0;
+#define TOUCH_ORIENTATION  LANDSCAPE
+int b=0,s,f;
+const float pi=3.14;
+extern uint8_t BigFont[];
+extern uint8_t SmallFont[];
+extern uint8_t SevenSegNumFont[];
+extern uint8_t UbuntuBold[];
+bool fl=0;
+int flg,flag=0;
+
+UTFT myGLCD(CTE50, 38, 39, 40, 41);
+UTFT_tinyFAT myFiles(&myGLCD);
+UTFT_Geometry geo(&myGLCD);
+//URTouch  myTouch( 6, 5, 4, 3, 2);
+
+String volLV="NA";
+String temp="NA";
+String acc1="NA";
+String acc2="NA";
+String bra="NA";
+String cur1="NA";
+String cur2="NA";
+String cur="NA";
+String vol="NA";
+String spe="000";
+String rpm="NA";
+String curLV="NA";
+String ambtemp="NA";
+float powerHV;
+float powerLV;
+float energyLV=0,p1=0;
+float energyHV=0,p2=0;
+float distance=0,p3=0;
+float SOCHV;
+float SOCLV;
+String str="";
+String scrape="";
+float voltageLV;
+float currentLV;
+float currentHV;
+float voltageHV;
+float tempint; 
+
+unsigned long long pasttime=0;
+unsigned long long curtime=0;
+unsigned long long t1=0;
+
+
+void dashsetup()
+{
+word res1,res2,res3;
+/*res1=myFiles.loadBitmap(156, 0, 488, 480,"IITR.RAW");
+   if (res1!=0)
+      {
+        if (res1==0x10)
+        {
+          myGLCD.print("File not found...", 0, 0);
+          myGLCD.print("IITR.RAW", 0, 14);
+        }
+        else
+        {
+          myGLCD.print("ERROR: ", 0, 0);
+          myGLCD.printNumI(res1, 56, 0);
+        }
+        delay(3000);
+        myGLCD.clrScr();
+      }
+      myGLCD.fillScr(VGA_WHITE);
+  res2=myFiles.loadBitmap(0, 105, 800,271 ,"IITRMS1.RAW");
+   if (res2!=0)
+      {
+        if (res2==0x10)
+        {
+          myGLCD.print("File not found...", 0, 0);
+          myGLCD.print("IITRMS1.RAW", 0, 14);
+        }
+        else
+        {
+          myGLCD.print("ERROR: ", 0, 0);
+          myGLCD.printNumI(res2, 56, 0);
+        }
+        delay(3000);
+        myGLCD.clrScr();
+      }
+      myGLCD.fillScr(VGA_WHITE);  */
+   res3=myFiles.loadBitmap(0, 0, 800,480 ,"spee800.RAW");
+ 
+   if (res3!=0)
+      {
+        if (res3==0x10)
+        {
+          myGLCD.print("File not found...", 0, 0);
+          myGLCD.print("spee800.RAW", 0, 14);
+        }
+        else
+        {
+          myGLCD.print("ERROR: ", 0, 0);
+          myGLCD.printNumI(res3, 56, 0);
+        }
+        delay(3000);
+        myGLCD.clrScr();
+      }
+myGLCD.setFont(UbuntuBold);
+myGLCD.print("HV",657,310);
+myGLCD.print("LV",102,310);
+
+}
+
+void Update(){
+  //long long loca=lTime_elapsed=millis();
+  String str="";
+  int strcount=0;
+    while(Serial.available()){
+      char ch1=Serial.read();
+      str+=ch1;}
+    flag=0;
+    flg=0;
+    strcount=str.length();
+   for(int i=0;i<strcount;i++)
+    {    
+     char ch=str.charAt(i);
+     if(ch<60&&ch>=45){
+       if(ch==':') {
+   
+      flg=1;
+      scr="";
+      continue;
+       }
+
+       else{
+  //delay(100);
+        if(flg==1){
+         if(ch=='/'){
+         flg++;
+         volLV=scr;
+         scr="";
+         continue; 
+        }
+        scr+=ch;
+       }
+    else if(flg==2){
+   if(ch=='/'){
+      flg++;
+      temp=scr;
+      scr="";
+      continue; 
+     }
+     scr+=ch;
+     }
+   else if(flg==3){
+   if(ch=='/'){
+      flg++;
+      acc1=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+
+   else if(flg==4){
+   if(ch=='/'){
+      flg++;
+      acc2=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+
+   else if(flg==5){
+   if(ch=='/'){
+      flg++;
+      bra=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+
+   else if(flg==6){
+   if(ch=='/'){
+      flg++;
+      cur1=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+   else if(flg==7){
+   if(ch=='/'){
+      flg++;
+      cur2=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+   else if(flg==8){
+   if(ch=='/'){
+      flg++;
+      cur=scr;
+      scr="";
+      continue; 
+     }
+
+   else if(flg==9){
+   if(ch=='/'){
+      flg++;
+      vol=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     } 
+      scr+=ch;
+     }
+
+   else if(flg==10){
+   if(ch=='/'){
+      flg++;
+      spe=scr;
+      scr="";
+      continue; 
+     }
+      scr+=ch;
+     }
+   else if(flg==11){
+   if(ch==';'){
+      flg=0;
+      rpm=scr;
+      scr="";
+      break; 
+     }
+      scr+=ch;
+     }
+   }  
+     }//charchk
+    } 
+
+    
+}
+void setup() {
+  // put your setup code here, to run once:
+//Serial.begin(9600);
+pinMode(7,OUTPUT);
+digitalWrite(7,LOW);
+Serial1.begin(9600);
+int picsize_x, picsize_y;
+myGLCD.InitLCD(); 
+myGLCD.clrScr();
+myGLCD.setFont(BigFont);
+//myTouch.InitTouch(TOUCH_ORIENTATION);
+myGLCD.fillScr(VGA_WHITE);
+file.initFAT();
+dashsetup();
+
+pasttime=millis();
+
+/*EEPROM.put(addr,p1);
+EEPROM.put(addr+sizeof(float),p2);
+EEPROM.put(addr+2*sizeof(float),p3);
+*/
+/*
+EEPROM.get(addr,p1);
+energyLV=p1;
+EEPROM.get(addr+sizeof(float),p2);
+energyHV=p2;
+EEPROM.get(addr+2*sizeof(float),p3);
+distance=p3;
+  */
+  
+}
+
+
+
+
+void sercomm(){
+//  t1=millis()-pasttime;
+   str="";
+   while (true)
+ {
+    
+   while (!Serial1.available())
+  {if(millis()-pasttime>500)
+   fl=0;
+       }
+      
+  char c=Serial1.read();
+  if(c=='a') {break;}
+  }
+
+
+while (true)
+ {
+    
+   while (!Serial1.available())
+  {
+    if(millis()-pasttime>500)
+   fl=0;
+       }
+  char c=Serial1.read();
+  if(c=='b') {break;}
+  str+=c;
+  }
+if(fl==0)
+pasttime=millis();
+
+  }
+
+
+  
+
+void printlcd()
+{
+// b = speed previous value
+if(b!=s)
+{
+  myGLCD.setColor(200,200,200);
+  geo.fillTriangle(395,230,405,230,400-130*cos(-1*pi/6.0+pi*b/120.0),230-130*sin(-1*pi/6.0+pi*b/120.0));
+  //myGLCD.drawBitmap(310,130,180,59,logo);
+  myGLCD.setColor(VGA_RED);
+  myGLCD.fillCircle(400,230,15);
+  geo.fillTriangle(395,230,405,230,400-130*cos(-1*pi/6.0+pi*s/120.0),230-130*sin(-1*pi/6.0+pi*s/120.0));
+  myGLCD.setColor(VGA_WHITE);
+  myGLCD.setFont(SevenSegNumFont);
+  myGLCD.printNumI(s,320,327);
+  myGLCD.setFont(BigFont);
+  myGLCD.print("Km/hr",420,350);
+  b=s;
+}
+
+/*myGLCD.setColor(22,26,33);
+myGLCD.fillRect(92,90,156,217);
+myGLCD.fillRect(640,90,711,217);
+*/
+myGLCD.setColor(VGA_WHITE);
+myGLCD.setBackColor(22,26,33);
+
+myGLCD.setFont(SmallFont);
+myGLCD.print("Current(A)",72,90); 
+myGLCD.print("Voltage(V)",72,130);
+myGLCD.print("Energy(Whr)",76,170);
+myGLCD.print("Current(A)",627,90);
+myGLCD.print("Voltage(V)",627,130);   
+myGLCD.print("Energy(Whr)",631,170);
+
+myGLCD.setFont(BigFont);
+myGLCD.print("    ",1,92,105);
+myGLCD.printNumF(currentLV,1,92,105,'.',4,'0');
+myGLCD.print("    ",1,92,145);
+myGLCD.printNumF(voltageLV,1,92,145,'.',4,'0');
+myGLCD.print("    ",92,185);
+myGLCD.printNumI(energyLV,92,185,4,'0');
+myGLCD.print("     ",1,640,105);
+myGLCD.printNumF(currentHV,1,640,105,'.',5,'0');
+myGLCD.print("    ",1,647,145); 
+myGLCD.printNumF(voltageHV,1,647,145,'.',4,'0');
+myGLCD.print("    ",647,185);    
+myGLCD.printNumI(energyHV,647,185,4,'0');
+
+myGLCD.setBackColor(VGA_BLACK);
+myGLCD.print("   ",715,413);
+myGLCD.printNumI(tempint,715,413,3,'0');
+myGLCD.print("     ",350,380);
+myGLCD.printNumF(distance,1,350,380,'.',5,'0');
+myGLCD.print("Km",440,380);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//str="A21.0B7.5C5.7D69E4F3G2H100I69J54K8L3A";
+//checkch();
+sercomm();
+//myGLCD.print(str,CENTER,40);
+if(tempint>40) digitalWrite(7,HIGH);
+else digitalWrite(7,LOW);
+update1();
+
+fl=1;
+
+//printcheck();
+  
+}
+
+void printcheck(){
+  myGLCD.print(" volLV= "+volLV,CENTER,0);
+  myGLCD.print(" temp= "+temp,CENTER,20);
+  myGLCD.print(" acc1= "+acc1,CENTER,40);
+  myGLCD.print(" acc2= "+acc2,CENTER,60);
+  myGLCD.print(" bra= "+bra,CENTER,80);
+ // myGLCD.print(" cur1= "+cur1,CENTER,100);
+//  myGLCD.print(" cur2= "+cur2,CENTER,120);
+  myGLCD.print(" cur= "+cur,CENTER,140);
+  myGLCD.print(" vol= "+vol,CENTER,160);
+  myGLCD.print("spe= "+spe,CENTER,180);
+  myGLCD.print(" rpm= "+rpm,CENTER,200);
+  myGLCD.print(" curLV= "+curLV,CENTER,220);
+ // myGLCD.print(" ambtemp= "+ambtemp,CENTER,240);
+  myGLCD.printNumF(powerLV,3,CENTER,260); 
+  myGLCD.printNumF( energyLV,3,CENTER,280);
+  myGLCD.printNumF(SOCLV,3,CENTER,300);
+  myGLCD.printNumF(powerHV,3,CENTER,320);
+  myGLCD.printNumF(energyHV,3,CENTER,340);
+  myGLCD.printNumF(SOCHV,3,CENTER,360);
+  
+  }
+
+
+
+float conv2float(String s1)
+{
+  if (s1.charAt(0)=='-') 
+  { 
+    s1=s1.substring(1,s1.length());
+    return -1.0*s1.toFloat();
+  }
+  else
+  { return s1.toFloat();}
+}
+
